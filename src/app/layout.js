6a0fc28b66af02f1951d9c2c4b8fc97ee414c752
abhinavbyjus23 <142393791+abhinavbyjus23@users.noreply.github.com>
2024-01-01 12:49:54 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>This will be shown in all pages</h1>
+        {children}</body>
     </html>
   )
 }
