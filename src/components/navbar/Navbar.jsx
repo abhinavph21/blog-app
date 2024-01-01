ef507745c4e28662d116476753a4d19989f39f63
abhinavbyjus23 <142393791+abhinavbyjus23@users.noreply.github.com>
2024-01-01 18:33:44 +0530

    links
    link to various routes

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Links from "./links/Links"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
+  )
+}
+export default Navbar
