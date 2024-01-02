369af93bee06dc62e1b6c53fe02636b9af5b2b2a
abhinavbyjus23 <142393791+abhinavbyjus23@users.noreply.github.com>
2024-01-02 12:21:57 +0530

    notFound-error page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
-  throw new Error("error in home page")
-  // return <div>Hello World!</div>;
+  // throw new Error("error in home page")      renders error page
+  return <div>Hello World!</div>;
 };
 
 export default Home;
