7426e68e47ba1680e10b29edfcd34a3750c0ccbc
abhinavbyjus23 <142393791+abhinavbyjus23@users.noreply.github.com>
2024-01-02 19:53:33 +0530

    Images in next.js external path config

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div >
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/13921072/pexels-photo-13921072.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill />
+      </div>
+    </div>
   )
 }
 
