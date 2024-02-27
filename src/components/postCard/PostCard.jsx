a2890ecd4b4a5d238839772fba582a6e365023b7
abhinavbyjus23 <142393791+abhinavbyjus23@users.noreply.github.com>
2024-02-27 19:12:07 +0530

    rename 4

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import styles from "./postCard.module.css"
+import Link from "next/link"
+
+const PostCard = ({ post }) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                {post?.img && <div className={styles.imgContainer}>
+                    <Image src={post.img} alt="" fill className={styles.img} />
+                </div>}
+                <div className={styles.date}>{new Date(post.createdAt).toString().slice(4, 16)}</div>
+            </div>
+            <div className={styles.bottom}>
+                {post.title && <h1 className={styles.title}>{post.title.length > 50 ? `${post.title.substring(0, 50)} ...` : post.title}</h1>}
+                {post.desc && <p className={styles.desc}>{post.desc.length > 80 ? `${post.desc.substring(0, 80)} ...` : post.desc}</p>}
+                <Link className={styles.link} href={`/blog/${post._id}`}>READ MORE</Link>
+            </div>
+        </div>
+    )
+}
+
+export default PostCard
